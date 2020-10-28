26ab01b489b48b24d35c812928e1cc09c89faeba
Andreas Poehlmann <andreas@poehlmann.io>
2020-10-29 00:12:20 +0100

    [pado_visualize] update app

--- pado_visualize/pado/ext/visualize/assets/custom-script.js
+++ pado_visualize/pado/ext/visualize/assets/custom-script.js
@@ -1,3 +1,9 @@
 
 console.log('If you see this alert, then your custom JavaScript script has run!');
 
+$(document).ready(() => {
+    $( "body" ).delegate( ".thumbnail-card", "click", function() {
+        console.log("yeah");
+        $.get( "http://127.0.0.1:9000" );
+    });
+});
