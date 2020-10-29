e61fa7f49f0ba7d181f189ee26871e116c99da87
Andreas Poehlmann <andreas@poehlmann.io>
2020-10-29 09:00:01 +0100

    [pado_visualize] add overlay to cards

--- pado_visualize/pado/ext/visualize/dashs/app_overview.py
+++ pado_visualize/pado/ext/visualize/dashs/app_overview.py
@@ -22,11 +22,15 @@ def render_preview_cards(pathname):
         img = html.Img(
             className="thumbnail", src=f"/thumbnails/{image_resource.id_str}.jpg"
         )
+        overlay = html.Img(
+            className="grid-overlay", src=f"/grid/{image_resource.id_str}.png"
+        )
         # title = html.H5("Card title", className="card-title")
         card = dbc.Card(
             [
                 # title,
                 img,
+                overlay,
             ],
             className="thumbnail-card",
         )
