02ae88f73ea077ef6fb75fe286f1384b115b557e
Andreas Poehlmann <andreas@poehlmann.io>
2020-11-04 20:44:43 +0100

    [pado_visualize] remove test js code

--- pado_visualize/assets/99-pado-visualize.js
+++ pado_visualize/assets/99-pado-visualize.js
@@ -4,6 +4,6 @@ console.log('If you see this alert, then your custom JavaScript script has run!'
 $(document).ready(() => {
     $( "body" ).delegate( ".thumbnail-card", "click", function() {
         console.log("yeah");
-        $.get( "http://127.0.0.1:9000" );
+        // $.get( "http://127.0.0.1:9000" );
     });
 });
