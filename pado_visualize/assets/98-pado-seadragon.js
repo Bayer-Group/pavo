7e4624263d46fbc850769bc397c698dd70811c80
Andreas Poehlmann <andreas@poehlmann.io>
2020-11-04 22:03:25 +0100

    [pado_visualize] use clientside callbacks

--- pado_visualize/assets/98-pado-seadragon.js
+++ pado_visualize/assets/98-pado-seadragon.js
@@ -1,32 +1,35 @@
-$(document).ready(function() {
-    let sources = $("#seadragon-container").attr("data-tilesources");
-
-    if (!sources) {
-        console.log("no tilesources set " + sources);
-        return;
+window.dash_clientside = Object.assign({}, window.dash_clientside, {
+    clientside: {
+        load_open_seadragon: function(url_pathname) {
+            if (!url_pathname.startsWith("/slide/")) {
+                console.log("no seadragon slide " + url_pathname);
+                return "";
+            }
+            let sources = url_pathname + "/image.dzi"
+            let viewer = new OpenSeadragon({
+                id: "seadragon-container",
+                tileSources: sources,
+                prefixUrl: "/slide",
+                showNavigator: true,
+                showRotationControl: true,
+                animationTime: 0.5,
+                blendTime: 0.1,
+                constrainDuringPan: true,
+                maxZoomPixelRatio: 2,
+                minZoomLevel: 1,
+                visibilityRatio: 1,
+                zoomPerScroll: 2,
+                timeout: 120000,
+            });
+            viewer.addHandler("open", function() {
+                // To improve load times, ignore the lowest-resolution Deep Zoom
+                // levels.  This is a hack: we can't configure the minLevel via
+                // OpenSeadragon configuration options when the viewer is created
+                // from DZI XML.
+                viewer.source.minLevel = 8;
+            });
+            console.log("initialized osd for " + sources);
+            return sources;
+        }
     }
-    var viewer = new OpenSeadragon({
-        id: "seadragon-container",
-            tileSources: sources,
-            prefixUrl: "/slide",
-            showNavigator: true,
-            showRotationControl: true,
-            animationTime: 0.5,
-            blendTime: 0.1,
-            constrainDuringPan: true,
-            maxZoomPixelRatio: 2,
-            minZoomLevel: 1,
-            visibilityRatio: 1,
-            zoomPerScroll: 2,
-            timeout: 120000,
-    });
-    viewer.addHandler("open", function() {
-        // To improve load times, ignore the lowest-resolution Deep Zoom
-        // levels.  This is a hack: we can't configure the minLevel via
-        // OpenSeadragon configuration options when the viewer is created
-        // from DZI XML.
-        viewer.source.minLevel = 8;
-    });
-
-    console.log("initialized osd for " + sources);
 });
--- pado_visualize/dashs/app_seadragon.py
+++ pado_visualize/dashs/app_seadragon.py
@@ -1,26 +1,19 @@
 # import dash
-import itertools
 
-import dash_bootstrap_components as dbc
-import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
-import plotly.express as px
-from flask import url_for
-from dash.dependencies import Input, Output
+from dash.dependencies import ClientsideFunction, Input, Output
+
 from pado_visualize.app import app
-from pado_visualize.dataloader import get_dataset
 
 
-@app.callback(
+app.clientside_callback(
+    ClientsideFunction(
+        namespace="clientside",
+        function_name="load_open_seadragon",
+    ),
     output=Output("seadragon-container", "data-tilesources"),
     inputs=[Input("url", "pathname")],
 )
-def view_slide(pathname):
-    if not pathname.startswith("/slide/"):
-        return ""
-    slide_url = f"{pathname}/image.dzi"
-    return slide_url
 
 
 layout = html.Div([
