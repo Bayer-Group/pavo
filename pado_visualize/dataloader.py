a0e59f4f8f1ab7e9974671b4fcfb2881f8c1fbad
Andreas Poehlmann <andreas@poehlmann.io>
2020-11-04 19:01:20 +0100

    Revert "[pado_visualize] fix buffer closed in send_file"
    This reverts commit 1419a101bc7a09fe294adbe4660b244fc46f4115.

--- pado_visualize/dataloader.py
+++ pado_visualize/dataloader.py
@@ -148,13 +148,14 @@ def serve_thumbnail_jpg(image_id):
     # save as image
     with io.BytesIO() as buffer:
         Image.fromarray(arr).save(buffer, format="JPEG")
-        buffer.seek(0)
-        return send_file(
-            buffer,
-            mimetype="image/jpeg",
-            as_attachment=True,
-            attachment_filename=f"{image_id}.jpg",
-        )
+        data = buffer.getvalue()
+
+    return send_file(
+        io.BytesIO(data),
+        mimetype="image/jpeg",
+        as_attachment=True,
+        attachment_filename=f"{image_id}.jpg",
+    )
 
 
 def wds_grid_thumbnail_array(wds_tar_path: Path):
@@ -213,10 +214,9 @@ def serve_grid_png(image_id):
 
     data = get_svs_thumbnail_filtered(p)
 
-    with io.BytesIO(data) as buffer:
-        return send_file(
-            buffer,
-            mimetype="image/png",
-            as_attachment=True,
-            attachment_filename=f"{image_id}.png",
-        )
+    return send_file(
+        io.BytesIO(data),
+        mimetype="image/png",
+        as_attachment=True,
+        attachment_filename=f"{image_id}.png",
+    )
