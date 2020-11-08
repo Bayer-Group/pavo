1f721a03d405dbc8ecd977c462df187a5abba577
Andreas Poehlmann <andreas@poehlmann.io>
2020-11-08 20:04:14 +0100

    [pado_visualize] refactor webdataset code (currently unused)

--- pado_visualize/__main__.py
+++ pado_visualize/__main__.py
@@ -6,13 +6,7 @@ def main():
 
     from pado_visualize.routes import init_routes
     from pado_visualize.data.dataset import init_dataset
-    from pado_visualize.dataloader import (
-        get_wds_map,
-        set_dataset,
-        set_dataset_from_store,
-        set_wds_dirs,
-        set_wds_map_from_store,
-    )
+    # from pado_visualize.data.webdataset import init_wds
     from pado_visualize.app import app
 
     # register all routes
@@ -23,17 +17,12 @@ def main():
     parser.add_argument("dataset_path", help="path to a pado dataset")
     args = parser.parse_args()
 
-    p = Path(args.dataset_path).expanduser().absolute().resolve()
-    # w = Path(args.wds_path).expanduser().absolute().resolve()
-
-    init_dataset(p, persist=True)
-
-        # if str(w) not in store:
-        #     print("getting wds_map")
-        #     # store[str(w)] = set_wds_dirs(w)
-        # else:
-        #     print("getting cached wds_map")
-        #     # set_wds_map_from_store(store[str(w)])
+    # dataset
+    _cache = Path("./pado_visualize.shelve")
+    p = Path(args.dataset_path)
+    init_dataset(p, persist=True, cache_file=_cache)
+    # w = Path(args.wds_path)
+    # init_webdataset(w, persist=True, cache_file=_cache)
 
     app.run_server(host="127.0.0.1", port=8080, debug=True)
 
---
+++ pado_visualize/data/webdataset.py
@@ -0,0 +1,121 @@
+import json
+import math
+import shelve
+import tarfile
+import warnings
+from pathlib import Path
+from typing import Dict, Optional
+
+import numpy as np
+from tqdm import tqdm
+
+__all__ = ["init_wds", "get_wds_map"]
+
+wds_map: Dict[str, Path] = {}
+
+
+# -- webdataset tars related functions --------------------------------
+
+def init_wds(
+    wds_path: Path,
+    persist: bool = True,
+    ignore_cache: bool = True,
+    cache_file: Optional[Path] = None,
+) -> None:
+    """initialize the webdataset map for the flask instance"""
+    global wds_map
+
+    if cache_file is None:
+        if persist:
+            warnings.warn("persist=True requested but cache_file is None")
+        persist = False
+
+    if not wds_path.is_dir():
+        raise NotADirectoryError(wds_path)
+
+    def load_wds(path):
+        # dont set this outside of init_webdataset
+        wm = {}
+        for wds_tar in tqdm(path.glob("*.tar"), desc="loading wds"):
+
+            if wds_tar.with_suffix(wds_tar.suffix + ".error").is_file():
+                warnings.warn(f"{wds_tar} ignored due to error")
+                continue
+
+            with tarfile.open(wds_tar, mode="r") as tar:
+                for tarinfo in tar:
+                    if not tarinfo.name.endswith(".json"):
+                        continue
+                    tile_dict = json.load(tar.extractfile(tarinfo))
+                    image_id = tile_dict["IMAGE"]
+                    break
+                else:
+                    image_id = None
+
+            if image_id is None:
+                warnings.warn(f"no image_id found for {wds_tar}")
+                continue
+            elif image_id in wm:
+                warnings.warn(f"{image_id}:{wds_tar} already in wds_map as {wm[image_id]}")
+                continue
+            else:
+                wm[image_id] = wds_tar
+        return wm
+
+    if not persist:
+        wds_map.update(load_wds(wds_path))
+
+    else:
+        with shelve.open(cache_file) as store:
+            key = str(wds_path)
+            if key not in store or ignore_cache:
+                print("loading web dataset from disk...")
+                wds = store[key] = load_wds(wds_path)
+            else:
+                print("loading webdataset from cache...", end=" ", flush=True)
+                wds = store[key]
+            wds_map.update(wds)
+    print("OK")
+
+
+def get_wds_map() -> Dict[str, Path]:
+    global wds_map
+    return wds_map
+
+
+# -- webdataset image related functions -------------------------------
+
+def wds_grid_thumbnail_array(wds_tar_path: Path) -> np.ndarray:
+    """return a thumbnail grid array from a tar file"""
+    arr = None
+
+    with tarfile.open(wds_tar_path, mode="r") as tar:
+        extractfile = tar.extractfile
+        json_load = json.load
+        for tarinfo in tar:
+            if not tarinfo.name.endswith(".json"):
+                continue
+
+            tile_dict = json_load(extractfile(tarinfo))
+            tile = tile_dict["tile"]
+            filt = tile_dict["filter"]
+            x, y = tile["slide_x"], tile["slide_y"]
+            tw, th = tuple(tile["size"])
+            ix, iy = x // tw, y // th
+
+            try:
+                arr[iy, ix] = filt["tissue_perc"]
+            except TypeError:
+                # this only happens on the first loop
+                sw, sh = tw + x + filt["dist_right"], th + y + filt["dist_bottom"]
+                aw = int(math.ceil(sw / tw))
+                ah = int(math.ceil(sh / th))
+                arr = np.zeros((ah, aw), dtype=np.float)
+                arr[iy, ix] = filt["tissue_perc"]
+
+    # make green channel
+    out = np.zeros((arr.shape[0], arr.shape[1], 4), dtype=np.uint8)
+    out[:, :, 1] = 255
+    out[:, :, 3] = 255.0 * arr
+
+    return out
