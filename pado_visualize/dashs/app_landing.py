a723b99594a5a448007f88a46a61a5b549d9a14d
Andreas Poehlmann <andreas@poehlmann.io>
2020-11-09 12:28:20 +0100

    landing page update

--- pado_visualize/dashs/app_landing.py
+++ pado_visualize/dashs/app_landing.py
@@ -2,21 +2,31 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 
 # from pado.ext.visualize.app import app
-from pado_visualize.utils import rowcol
+from pado_visualize.dash_pado_components import RowCol
 
 layout = dbc.Container(
-    rowcol(
+    RowCol([
         dbc.Jumbotron(
             [
-                html.H1("Visualize your dataset", className="display-3"),
                 html.P(
-                    "pado let's you visualize the metadata, image data "
-                    "and annotations in your PadoDataset.",
-                    className="lead",
+                    "Explore the metadata, image data, annotations and predictions stored in your PadoDataset.",
                 ),
-                html.P(dbc.Button("Open Dataset", color="primary"), className="lead"),
-            ]
+            ],
+            className="pado-jumbotron",
         )
-    ),
+    ]),
     id="landing-container"
+)
+
+layout_404 = dbc.Container(
+    RowCol([
+        dbc.Jumbotron(
+            [
+                html.H1("404"),
+                html.P("page or resource not found."),
+            ],
+            className="pado-jumbotron",
+        )
+    ]),
+    id="error-container"
 )
