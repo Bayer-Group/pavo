c878b656f65f677759c877c7a9f63f3dfe4604c9
Andreas Poehlmann <andreas@poehlmann.io>
2020-11-09 12:28:45 +0100

    pado_visualize: comment unused js

--- pado_visualize/assets/99-pado-visualize.js
+++ pado_visualize/assets/99-pado-visualize.js
@@ -7,10 +7,12 @@ $(document).ready(() => {
         // $.get( "http://127.0.0.1:9000" );
     });
 
+  /*
     $( "body" ).delegate( ".pado-body", "mousemove", function(e) {
         $(this).css({backgroundPositionX: (e.clientX * 0.01) + "px"});
         console.log(e.clientX, e.clientY);
     });
+  */
 
 });
 
