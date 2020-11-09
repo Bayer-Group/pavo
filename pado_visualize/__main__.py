8ccdda2f4861e233edc33fd73e356c3fc34726e8
Andreas Poehlmann <andreas@poehlmann.io>
2020-11-09 12:56:26 +0100

    pado_visualize.__main__: separate data init from app init

--- pado_visualize/__main__.py
+++ pado_visualize/__main__.py
@@ -1,28 +1,42 @@
+from pathlib import Path
 
-def main():
-    import argparse
-    from pathlib import Path
 
-    from pado_visualize.routes import init_routes
+def init_data(dataset_path: str, cache_path: str):
     from pado_visualize.data.dataset import init_dataset
+
+    # dataset
+    _cache = Path(cache_path)
+    p = Path(dataset_path)
+    init_dataset(p, persist=True, cache_file=_cache)
+
+    # w = Path(args.wds_path)
     # from pado_visualize.data.webdataset import init_wds
+    # init_webdataset(w, persist=True, cache_file=_cache)
+
+
+def init_app():
+    from pado_visualize.routes import init_routes
     from pado_visualize.app import app
 
     # register all routes
     init_routes()
 
+    # spawn app
+    return app
+
+
+def main():
+    import argparse
+
+    # parse commandline
     parser = argparse.ArgumentParser()
-    # parser.add_argument("--wds-path", help="path to wds path folder")
     parser.add_argument("dataset_path", help="path to a pado dataset")
     args = parser.parse_args()
 
-    # dataset
-    _cache = Path("./.pado_visualize.shelve")
-    p = Path(args.dataset_path)
-    init_dataset(p, persist=True, cache_file=_cache)
-    # w = Path(args.wds_path)
-    # init_webdataset(w, persist=True, cache_file=_cache)
+    init_data(args.dataset_path, cache_path="./.pado_visualize.shelve")
+    app = init_app()
 
+    # run dev server
     app.run_server(host="127.0.0.1", port=8080, debug=True)
 
 
