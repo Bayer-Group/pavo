8dec7e2fb9ef7e25584361f02dd1f8aa3485da30
Andreas Poehlmann <andreas@poehlmann.io>
2020-11-09 16:35:12 +0100

    pado_visualize: remove unused javascript

--- pado_visualize/assets/99-pado-visualize.js
+++ pado_visualize/assets/99-pado-visualize.js
@@ -1,18 +1,4 @@
 
-console.log('If you see this alert, then your custom JavaScript script has run!');
-
 $(document).ready(() => {
-    $( "body" ).delegate( ".thumbnail-card", "click", function() {
-        console.log("yeah");
-        // $.get( "http://127.0.0.1:9000" );
-    });
-
-  /*
-    $( "body" ).delegate( ".pado-body", "mousemove", function(e) {
-        $(this).css({backgroundPositionX: (e.clientX * 0.01) + "px"});
-        console.log(e.clientX, e.clientY);
-    });
-  */
-
+    console.log('If you see this alert, then your custom JavaScript script has run!');
 });
-
