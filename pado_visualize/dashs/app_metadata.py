08a847f2cce1ff3d5b9613ae141636d56894c365
Andreas Poehlmann <andreas@poehlmann.io>
2020-11-26 13:05:24 +0100

    add interactive plot in metadata app

--- pado_visualize/dashs/app_metadata.py
+++ pado_visualize/dashs/app_metadata.py
@@ -27,7 +27,9 @@ def _plot_card_bar(x, y, title=None):
             "showlegend": False,
         }
     )
-    return go.Figure(data=bar_data, layout=bar_layout)
+    fig = go.Figure(data=bar_data, layout=bar_layout)
+    fig.update_layout(clickmode='event+select')
+    return fig
 
 
 layout = dbc.Container([
@@ -65,11 +67,21 @@ layout = dbc.Container([
     ]),
     dbc.Row([
         dbc.Col([
+            html.Div([], id="selected-study-container")
+        ], xs=12),
+    ]),
+    dbc.Row([
+        dbc.Col([
             PlotCard(
                 figure_id="fig-findings",
                 title="Findings",
                 text="Slides with findings in dataset",
             ),
+        ], xs=12)
+    ]),
+    dbc.Row([
+        dbc.Col([
+            html.Div([], id="selected-finding-container")
         ], xs=12)
     ]),
 ])
@@ -142,3 +154,151 @@ def update_barchart_findings(pathname, data):
     except KeyError:
         pass
     return _plot_card_bar(x=counts.index[:20], y=counts.values[:20])
+
+
+def _plot_summary_studies(df):
+    title = "Selected Study Summary"
+    selection = ", ".join(df["STUDY"].unique())
+    text = f"Studies: {selection}"
+
+    bar_data = []
+    doses = (
+        df[
+            ["GROUP__DOSE", "GROUP__DOSE_UNIT"]
+        ].set_index(
+            df["GROUP__DOSE"]
+        ).apply(
+            lambda row: ' '.join(row.values.astype(str)), axis=1
+        ).drop_duplicates(
+        ).sort_index()
+    )
+    for finding, _df in df.groupby("FINDING")["GROUP__DOSE"]:
+        if finding == "UNREMARKABLE":
+            continue
+        data = _df.value_counts().sort_index(ascending=True)
+        cnts = []
+        for d in doses.index:
+            try:
+                v = data[d]
+            except KeyError:
+                v = 0
+            cnts.append(v)
+
+        bar_data.append(
+            go.Bar(name=finding, x=doses, y=cnts)
+        )
+
+    if not bar_data:
+        return ""
+
+    bar_layout = go.Layout(
+        {
+            "yaxis": {},
+            "xaxis": {
+                "showticklabels": True
+            },
+            "height": 320,
+            "margin": go.layout.Margin(l=0, r=0, t=0.2, b=0),
+            "showlegend": True,
+            "legend": {'orientation': "h", 'yanchor': "bottom", 'y': 1.05, 'xanchor': "center", 'x': 0.5}
+        }
+    )
+    fig = go.Figure(data=bar_data, layout=bar_layout)
+
+    graph = dcc.Graph(figure=fig)
+    content = [
+        dbc.CardHeader([
+            html.H5(title, className="card-title"),
+        ]),
+        dbc.CardBody([graph]),
+        dbc.CardFooter([
+            html.P(text, className="card-text"),
+        ]),
+    ]
+    return dbc.Card(content, className="plot-card summary")
+
+
+def _plot_summary_findings(df):
+    title = "Selected Finding Summary"
+    selection = ", ".join(df["FINDING"].unique())
+    text = f"Findings: {selection}"
+
+    bar_data = []
+    for compound, _df in df.groupby("COMPOUND")["GROUP__DOSE_LEVEL"]:
+        data = _df.value_counts().sort_index(ascending=True)
+        bar_data.append(
+            go.Bar(name=compound, x=data.index, y=data)
+        )
+
+    if not bar_data:
+        return ""
+
+    bar_layout = go.Layout(
+        {
+            "yaxis": {},
+            "xaxis": {
+                "showticklabels": True
+            },
+            "height": 320,
+            "margin": go.layout.Margin(l=0, r=0, t=0.2, b=0),
+            "showlegend": True,
+            "legend": {'orientation': "h", 'yanchor': "bottom", 'y': 1.05, 'xanchor': "center", 'x': 0.5}
+        }
+    )
+    fig = go.Figure(data=bar_data, layout=bar_layout)
+
+    graph = dcc.Graph(figure=fig)
+    content = [
+        dbc.CardHeader([
+            html.H5(title, className="card-title"),
+        ]),
+        dbc.CardBody([graph]),
+        dbc.CardFooter([
+            html.P(text, className="card-text"),
+        ]),
+    ]
+    return dbc.Card(content, className="plot-card summary")
+
+
+@app.callback(
+    output=Output("selected-finding-container", "children"),
+    inputs=[
+        Input("fig-findings", "selectedData"),
+        Input("subset-filter-store", "data"),
+    ],
+)
+def display_selected_finding_data(selectedData, data):
+    if not selectedData:
+        return ""
+
+    selected_findings = [pt.get("label") for pt in selectedData.get("points", [])]
+    selected_findings = [f for f in selected_findings if f]
+
+    if not selected_findings:
+        return ""
+
+    data.setdefault("FINDING", []).extend(selected_findings)
+    df = get_metadata(filter_dict=data)
+    return _plot_summary_findings(df)
+
+
+@app.callback(
+    output=Output("selected-study-container", "children"),
+    inputs=[
+        Input("fig-studies", "selectedData"),
+        Input("subset-filter-store", "data"),
+    ],
+)
+def display_selected_study_data(selectedData, data):
+    if not selectedData:
+        return ""
+
+    selected_studies = [pt.get("label") for pt in selectedData.get("points", [])]
+    selected_studies = [f for f in selected_studies if f]
+
+    if not selected_studies:
+        return ""
+
+    data.setdefault("STUDY", []).extend(selected_studies)
+    df = get_metadata(filter_dict=data)
+    return _plot_summary_studies(df)
