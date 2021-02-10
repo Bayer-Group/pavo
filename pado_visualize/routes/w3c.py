613964e6ba79f2e0b16e0c9ac8aff6e65e32cde5
Andreas Poehlmann <andreas@poehlmann.io>
2021-02-10 15:36:15 +0100

    pado_visualize: support svg path annotations

--- pado_visualize/routes/w3c.py
+++ pado_visualize/routes/w3c.py
@@ -1,5 +1,9 @@
+from __future__ import annotations
+
+import re
 import uuid
 from typing import List
+from typing import TYPE_CHECKING
 from urllib.parse import unquote
 
 from flask import jsonify, abort
@@ -9,9 +13,27 @@ from pado.images import ImageId
 from pado_visualize.app import app
 from pado_visualize.data.dataset import get_dataset
 
+if TYPE_CHECKING:
+    from shapely.geometry.base import BaseGeometry
 
-def w3c_like_annotation(class_name, region, r_g_b=None):
-    """make a w3c annotation like annotation"""
+
+# remove style information from svg for w3c annotation
+_svg_style_re = re.compile(
+    r'(fill|stroke)="#[0-9a-f]{6}" ?'
+    r'|fill-rule="evenodd" ?'
+    r'|(stroke-width|opacity)="[0-9]*([.][0-9]*)?" ?'
+)
+
+
+def w3c_like_annotation(class_name: str, region: BaseGeometry):
+    """make a w3c annotation like annotation
+
+    see: https://www.w3.org/TR/annotation-vocab/#annotation
+
+    """
+    svg_path = region.svg()
+    # strip all style information
+    svg_path = _svg_style_re.sub("", svg_path)
     return {
         "@context": "http://www.w3.org/ns/anno.jsonld",
         "id": f"#{uuid.uuid4()}",
@@ -20,10 +42,14 @@ def w3c_like_annotation(class_name, region, r_g_b=None):
             "type": "TextualBody",
             "value": class_name,
         }],
+        "motivation": "classifying",
+        "creator": None,
+        "created": None,
         "target": {
             "selector": {
                 "type": "SvgSelector",
-                "value": f"<svg>{region.svg()}</svg>",
+                # https://www.w3.org/TR/annotation-model/#svg-selector
+                "value": f"<svg>{svg_path}</svg>",
             }
         }
     }
