4f8873b9a7c9b74383e75c93216dd61cde547fc3
Andreas Poehlmann <andreas@poehlmann.io>
2021-02-10 16:18:02 +0100

    pado_visualize: wip include basic auth

--- pado_visualize/__main__.py
+++ pado_visualize/__main__.py
@@ -14,6 +14,7 @@ def main():
     parser.add_argument("--build-qpzip-cache", action="store_true")
     parser.add_argument("--rebuild-cache", action="store_true")
     parser.add_argument("--port", type=int, default=8000)
+    parser.add_argument("--no-auth", action="store_true", help="disable basic authentication")
     args = parser.parse_args()
 
     init_data(args.dataset_path, cache_path="./.pado_visualize.cache", rebuild=args.rebuild_cache)
@@ -27,6 +28,12 @@ def main():
         from pado_visualize.routes.qpzip import _build_qpzip_cache
         _build_qpzip_cache()
         return 0
+
+    if args.no_auth:
+        print("RUNNING THE APP WITHOUT AUTHENTICATION", file=sys.stderr)
+    else:
+        from dash_auth import BasicAuth
+        BasicAuth(app, {"user": "pass"})  # fixme
 
     # run dev server
     return app.run_server(host="127.0.0.1", port=args.port, debug=args.debug)
