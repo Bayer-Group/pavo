321d238cedb178f0afda1e0501aee3775657ac38
Andreas Poehlmann <andreas@poehlmann.io>
2021-02-10 20:28:22 +0100

    pado_visualize: setup logging early in app.py

--- pado_visualize/app.py
+++ pado_visualize/app.py
@@ -1,9 +1,32 @@
-"""dash instance submodule
+"""dash instance submodule"""
+# prevent circular imports by providing Dash instance in its own submodule
+import logging
+import sys
+from logging.config import dictConfig
 
-to prevent circular imports provide Dash instance in its own submodule
-"""
 import dash
 import dash_bootstrap_components as dbc
+
+
+# --- configure logging handlers as early as possible -----------------
+
+dictConfig({
+    'version': 1,
+    'root': {
+        'level': 'INFO',
+    },
+})
+
+for logger_name in ['pado_visualize.data.dataset']:
+    logger = logging.getLogger(logger_name)
+    backend_handler = logging.StreamHandler(sys.stderr)
+    backend_handler.setFormatter(
+        logging.Formatter('%(name)s::%(levelname)s: %(message)s')
+    )
+    logger.addHandler(backend_handler)
+
+
+# === instantiate application instance ================================
 
 app = dash.Dash(
     __name__,
