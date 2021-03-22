d649080fb2ad9e0b8ecf23da918caa744e05a490
Andreas Poehlmann <andreas@poehlmann.io>
2021-03-22 23:20:05 +0100

    pado_visualize: side menu design

--- pado_visualize/home/views.py
+++ pado_visualize/home/views.py
@@ -8,5 +8,5 @@ blueprint = Blueprint('home', __name__, url_prefix='/')
 @blueprint.route("/")
 @blueprint.route("/index.htm")
 @blueprint.route("/index.html")
-def home():
-    return render_template("home/index.html")
+def index():
+    return render_template("home/index.html", page_title="Home")
