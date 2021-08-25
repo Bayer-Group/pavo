5fdea875e0e2390c5d78e34eb0ce820c01c371a2
Andreas Poehlmann <andreas@poehlmann.io>
2021-08-25 23:39:51 +0200

    pado_visualize.extensions: add custom JSGlue

--- pado_visualize/extensions.py
+++ pado_visualize/extensions.py
@@ -1,7 +1,14 @@
 """flask extensions custom to pado_visualize"""
+import json
+
 from celery import Celery
-from flask_caching import Cache
+from celery import states
 from flask import Flask
+from flask import current_app
+from flask import render_template
+from flask_caching import Cache
+from flask_jsglue import JSGlue
+from flask_jsglue import get_routes
 
 __all__ = [
     "cache",
@@ -33,6 +40,9 @@ def register_extensions(app: Flask, *, is_worker: bool = False) -> None:
         from pado_visualize.utils import url_for_versioned
         app.jinja_env.globals['url_for_versioned'] = url_for_versioned
 
+        # register custom JSGlue
+        ServerRoutesJS().init_app(app)
+
 
 # --- constants ---
 
@@ -62,3 +72,25 @@ class TaskState:
     @classmethod
     def is_propagated(cls, state):
         return state in states.PROPAGATE_STATES
+
+
+# --- extras ---
+
+class ServerRoutesJS(JSGlue):
+
+    def init_app(self, app):
+
+        @app.route("/server_routes.js")
+        def serve_js():
+            return self.generate_js(), 200, {'Content-Type': 'text/javascript'}
+
+        @app.context_processor
+        def context_processor():
+            return {type(self).__name__: type(self)}
+
+    def generate_js(self):
+        routes = get_routes(current_app or self.app)
+        # noinspection PyUnresolvedReferences
+        return render_template(
+            "jsglue/js_bridge.js", namespace=type(self).__name__, rules=json.dumps(routes)
+        )
