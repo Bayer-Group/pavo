1a9b65115883bc61dc9f3ab6017ede1ff2e83e8b
Jake Pencharz <jake.jay.p@gmail.com>
2021-10-13 13:09:44 +0200

    Implement endpoint to interact with annotation style predictions.

--- pado_visualize/api/api.py
+++ pado_visualize/api/api.py
@@ -1,5 +1,6 @@
-from flask import request, Blueprint
+from flask import request, Blueprint, jsonify
 
+from pado.images import ImageId
 from pado.annotations import Annotation
 
 from pado_visualize.data import dataset
@@ -8,40 +9,63 @@ from pado_visualize.data import dataset
 blueprint = Blueprint('api', __name__)
 
 
-@blueprint.route("/<var>/sanity_check")
-def sanity_check(var):
-    return f'you are sane: {var}', 200
-
+# TODO: implement tests for this endpoint
+# TODO: add authorisation
 @blueprint.route("/<image_id:image_id>/predictions", methods=['GET', 'POST'])
 def manage_predictions(image_id):
     """endpoint to manipulate image predictions"""
 
+    prediction_types = ('annotation', 'image')
+
     if request.method == 'POST':
-        """inserts an annotation into a dataset"""
-        prediction_types = ('annotation', 'image')
-        payload = request.get_json()
+        data = request.get_json()
         try:
-            prediction_type = payload['prediction_type']
-            assert prediction_type in prediction_types, 'Invalid prediction type.'
-
-            prediction_record = payload['prediction']
-
+            prediction_type = data['prediction_type']
+            prediction_record = data['prediction']
         except KeyError as e:
-            return f'KeyError on {e}', 400
-        except AssertionError as e:
-            return f'{e}', 400
-
-        a = Annotation.from_obj(prediction_record)
-
-        print(len(dataset.annotations[image_id].df))
-        dataset.annotations[image_id].insert(index=0, value=a)
-        print(len(dataset.annotations[image_id].df))
-
-        return f'', 200
+            return f'KeyError: {e} is required.', 400
+        
+        if prediction_type not in prediction_types:
+            return f'Prediction type "{prediction_type}" is invalid.', 400
+        
+        if prediction_type == "annotation":
+            return _insert_annotation_prediction(prediction_record, image_id)
+        else:
+            return _insert_image_prediction()
     
     elif request.method == 'GET':
-        """return all predictions for an image"""
+        """return all predictions for an image
+        
+        Note: currently only annotation style predictions have been implemented.
+        Once image style predictions are supported this endpoint should be able
+        to return a collection of one/both prediction types.
+        
+        # TODO: allow filtering by annotation type
+        """
 
-        # TODO: figure out how to filter annotations by annotator.type=='model'
-        # TODO: further filter by predictions by prediction types
-        return str(dataset.annotations[image_id]), 200
+        annotations_df = dataset.annotations[image_id].df
+        mask = annotations_df['annotator'].apply(lambda x: x.get('type') == 'model')
+        predictions_df = annotations_df[mask]
+
+        return jsonify(predictions_df.to_dict(orient='records')), 200
+
+# ---- helper functions --------------------------------------------------------
+def _insert_annotation_prediction(prediction_record: dict, image_id: ImageId):
+    """inserts an annotation style prediction into an image's list of annotations"""
+
+    if prediction_record['annotator']['type'] != 'model':
+        return f'Annotator type for predictions must be "model"', 400 
+
+    try:
+        dataset.annotations[image_id].insert(
+            index=0, 
+            value=Annotation.from_obj(prediction_record)
+        )
+    except Exception as e:
+        return f'Could not insert prediction for {image_id} due to {e}', 500
+
+    return '', 200
+
+def _insert_image_prediction():
+    # TODO: upload a large image style prediction here
+    return 'not implemented', 200
