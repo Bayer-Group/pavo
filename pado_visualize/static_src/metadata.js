32e6f15a591fb721f2859b6902cd35aeb40d0d2e
Andreas Poehlmann <andreas@poehlmann.io>
2021-11-29 18:06:23 +0100

    pado_visualize: install lineup via npm
    Couldn't get LineUpJS 4.3.0 to run...

--- pado_visualize/static_src/metadata.js
+++ pado_visualize/static_src/metadata.js
@@ -1,13 +1,7 @@
 import "./_base.js";
 import "./css/metadata.scss";
 
-/** you won this time webpack *shaking fist* */
-// import "./vendor/lineupjs/0a90cd2d51265d96e0f3.ttf";
-// import "./vendor/lineupjs/7fe2c712e328094b844b.woff";
-// import "./vendor/lineupjs/8d1e317a83ec2b6cddd4.svg";
-// import "./vendor/lineupjs/dee14f29dff532d4a714.woff2";
-// import "./vendor/lineupjs/LineUpJS.css";
-// import * as LineUpJS from "./vendor/lineupjs/LineUpJS.js";
+import * as LineUpJS from "lineupjs";
 
 /**
  * setup our lineup viewer
