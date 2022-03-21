231bccf427d6394e110b25c13801827c35647311
Andreas Poehlmann <andreas@poehlmann.io>
2022-03-21 07:41:22 +0100

    pado_visualize: fix toggle 2

--- pado_visualize/static_src/slides_openseadragon.js
+++ pado_visualize/static_src/slides_openseadragon.js
@@ -119,7 +119,7 @@ function getTileSource(viewer_id, idx) {
   const viewer = getViewer(viewer_id);
   for (let j = 0; j < viewer.world.getItemCount(); j++) {
     let ts = viewer.world.getItemAt(j);
-    if (ts.source.queryParams.input === tileSources[Number(idx)].tileSource) {
+    if (ts.source.queryParams.input === window.imagePredictionTileSources[Number(idx)].tileSource) {
       return ts;
     }
   }
