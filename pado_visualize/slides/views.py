56449d902ccfdc723debc705ae62dc6b0e33d7d5
Andreas Poehlmann <andreas@poehlmann.io>
2022-03-25 01:22:02 +0100

    pado_visualize: strip storage_options 'profile' from image urlpaths

--- pado_visualize/slides/views.py
+++ pado_visualize/slides/views.py
@@ -12,12 +12,16 @@ from flask import make_response
 from flask import render_template
 from flask import request
 from flask import send_file
-
 from pado.annotations import Annotation
 from pado.annotations import Annotations
 from pado.images.providers import image_cached_percentage
 from pado.images.providers import image_is_cached_or_local
-from pado.io.files import urlpathlike_to_fsspec
+from pado.io.files import fsopen
+from pado.io.files import urlpathlike_get_fs_cls
+from pado.io.files import urlpathlike_get_path
+from pado.io.files import urlpathlike_get_storage_args_options
+from tiffslide.deepzoom import MinimalComputeAperioDZGenerator
+
 from pado_visualize.data import DatasetState
 from pado_visualize.data import dataset
 from pado_visualize.extensions import cache
@@ -28,7 +32,6 @@ from pado_visualize.slides.utils import thumbnail_image
 from pado_visualize.utils import check_numeric_list
 from pado_visualize.utils import int_ge_0
 from pado_visualize.utils import int_ge_1
-from tiffslide.deepzoom import MinimalComputeAperioDZGenerator
 
 if TYPE_CHECKING:
     from pado.images import ImageId
@@ -36,13 +39,14 @@ if TYPE_CHECKING:
 
 
 # view blueprint for slide endpoints
-blueprint = Blueprint('slides', __name__)
+blueprint = Blueprint("slides", __name__)
 
 
 @blueprint.before_request
 @dataset.requires_state(DatasetState.READY, abort, 404, "dataset is not initialized")
 def dataset_ready():
     pass
+
 
 def _unpack_filter_params(request):
     page = request.args.get("page", 0, type=int_ge_0)
@@ -51,9 +55,11 @@ def _unpack_filter_params(request):
     if page_size not in allowed_page_sizes:
         abort(403, f"page_size must be one of {allowed_page_sizes!r}")
     filter = {
-        'filename': request.args.get('filename', None),
-        'metadata_key': request.args.get('metadata_key', None),
-        'metadata_values': check_numeric_list(request.args.getlist('metadata_values', None)),
+        "filename": request.args.get("filename", None),
+        "metadata_key": request.args.get("metadata_key", None),
+        "metadata_values": check_numeric_list(
+            request.args.getlist("metadata_values", None)
+        ),
     }
 
     return page, page_size, filter
@@ -63,10 +69,7 @@ def _unpack_filter_params(request):
 def index():
     page, page_size, filter = _unpack_filter_params(request)
     page_images = get_paginated_images(
-        dataset, 
-        page=page, 
-        page_size=page_size,
-        filter=filter
+        dataset, page=page, page_size=page_size, filter=filter
     )
 
     return render_template(
@@ -79,14 +82,12 @@ def index():
         metadata_attributes=get_all_metadata_attribute_options(),
     )
 
-@blueprint.route("/thumbnails", methods=['GET'])
+
+@blueprint.route("/thumbnails", methods=["GET"])
 def thumbnails():
     page, page_size, filter = _unpack_filter_params(request)
     page_images = get_paginated_images(
-        dataset, 
-        page=page, 
-        page_size=page_size,
-        filter=filter
+        dataset, page=page, page_size=page_size, filter=filter
     )
 
     return render_template(
@@ -99,31 +100,33 @@ def thumbnails():
         metadata_attributes=get_all_metadata_attribute_options(),
     )
 
+
 @blueprint.route("/thumbnail_<image_id:image_id>_<int:size>.jpg")
 def thumbnail(image_id: ImageId, size: int):
     if size not in {100, 200}:
         return abort(403, "thumbnail size not in {100, 200}")
 
     fs, path = thumbnail_fs_and_path(image_id, size)
-    assert fs.protocol == "file", "we assume local cache for now"    
+    assert fs.protocol == "file", "we assume local cache for now"
     try:
         return send_file(
             path,
-            mimetype='image/jpeg',
+            mimetype="image/jpeg",
             as_attachment=True,
-            download_name=request.path.split("/")[-1]
+            download_name=request.path.split("/")[-1],
         )
     except FileNotFoundError:
         thumbnail_image(image_id, dataset.images[image_id])
         return send_file(
             path,
-            mimetype='image/jpeg',
+            mimetype="image/jpeg",
             as_attachment=True,
-            download_name=request.path.split("/")[-1]
+            download_name=request.path.split("/")[-1],
         )
 
 
 # --- viewer endpoints ------------------------------------------------
+
 
 @blueprint.route("/viewer/<image_id:image_id>/osd")
 def viewer_openseadragon(image_id: ImageId):
@@ -142,7 +145,7 @@ def viewer_openseadragon(image_id: ImageId):
         image_predictions.append({"idx": idx, "name": name, "tooltip": tooltip})
 
     # metadata
-    metadata = {"name": ['/'.join(image_id.parts)]}
+    metadata = {"name": ["/".join(image_id.parts)]}
     mdf = dataset.metadata.get(image_id, None)
     if mdf is not None and not mdf.empty:
         for col in mdf:
@@ -170,32 +173,31 @@ def cache_status(image_id: ImageId):
     """return a json status message about the current cache state"""
     cache_inactive = current_app.config.get("CACHE_IMAGES_PATH", None) is None
     data = {
-        'cache': 'inactive' if cache_inactive else 'active',
+        "cache": "inactive" if cache_inactive else "active",
     }
 
     try:
         image = dataset.images[image_id]
     except KeyError:
-        return {'status': 404, 'ready': False, 'pct_cached': 0.0, **data}
+        return {"status": 404, "ready": False, "pct_cached": 0.0, **data}
 
     if cache_inactive:
-        return {'status': 200, 'ready': True, 'pct_cached': 0.0, **data}
+        return {"status": 200, "ready": True, "pct_cached": 0.0, **data}
 
     elif image_is_cached_or_local(image):
-        return {'status': 200, 'ready': True, 'pct_cached': 100.0, **data}
+        return {"status": 200, "ready": True, "pct_cached": 100.0, **data}
 
     else:
         pct_cached = image_cached_percentage(image)
-        return {'status': 200, 'ready': False, 'pct_cached': pct_cached, **data}
+        return {"status": 200, "ready": False, "pct_cached": pct_cached, **data}
 
 
 # --- pyramidal tile server -------------------------------------------
 
+
 @cache.memoize()
 def _slide_get_deep_zoom_from_session(
-    image_id: ImageId,
-    *,
-    image_prediction_idx: int | None = None
+    image_id: ImageId, *, image_prediction_idx: int | None = None
 ) -> MinimalComputeAperioDZGenerator:
     """retrieve the deep zoom generator from the user session"""
     if image_prediction_idx is None:
@@ -205,13 +207,17 @@ def _slide_get_deep_zoom_from_session(
         _ipp: ImagePredictionProvider = dataset.predictions.images
         urlpath = _ipp[image_id][image_prediction_idx].image.urlpath
 
-    of = urlpathlike_to_fsspec(urlpath)
+    fs_cls = urlpathlike_get_fs_cls(urlpath)
+    path = urlpathlike_get_path(urlpath, fs_cls=fs_cls)
+    args, storage_options = urlpathlike_get_storage_args_options(urlpath)
+    storage_options.pop("profile", None)
+    fs = fs_cls(*args, **storage_options)
     # noinspection PyTypeChecker,PydanticTypeChecker
-    dzi = MinimalComputeAperioDZGenerator(of)
+    dzi = MinimalComputeAperioDZGenerator(fsopen(fs, path))
     return dzi
 
 
-@blueprint.route('/viewer/<image_id:image_id>/osd/image.dzi')
+@blueprint.route("/viewer/<image_id:image_id>/osd/image.dzi")
 def slide_dzi(image_id: ImageId):
     ip_idx = request.args.get("image_prediction_idx", default=None, type=int_ge_0)
     try:
@@ -219,11 +225,13 @@ def slide_dzi(image_id: ImageId):
     except (KeyError, FileNotFoundError) as err:
         return abort(404, str(err))
     resp = make_response(dz.get_dzi())
-    resp.mimetype = 'application/xml'
+    resp.mimetype = "application/xml"
     return resp
 
 
-@blueprint.route('/viewer/<image_id:image_id>/osd/image_files/<int:level>/<int:col>_<int:row>.jpeg')
+@blueprint.route(
+    "/viewer/<image_id:image_id>/osd/image_files/<int:level>/<int:col>_<int:row>.jpeg"
+)
 def slide_tile(image_id, level, col, row):
     ip_idx = request.args.get("image_prediction_idx", default=None, type=int_ge_0)
     try:
@@ -243,13 +251,14 @@ def slide_tile(image_id, level, col, row):
         return abort(404, "not implemented tile request")
 
     resp = make_response(tile)
-    resp.mimetype = 'image/jpeg'
+    resp.mimetype = "image/jpeg"
     return resp
 
 
 # --- annotation viewer -------------------------------------------
 
-@blueprint.route('/viewer/<image_id:image_id>/annotations.geojson')
+
+@blueprint.route("/viewer/<image_id:image_id>/annotations.geojson")
 def serve_geojson_annotations(image_id):
     try:
         annotations: Annotations = dataset.annotations[image_id]
@@ -258,27 +267,26 @@ def serve_geojson_annotations(image_id):
 
     data = [
         {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Polygon',
-                'coordinates': [list(annotation.geometry.exterior.coords)],
+            "type": "Feature",
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [list(annotation.geometry.exterior.coords)],
             },
-            'properties': {
-                'classification': {
-                    'name': annotation.classification,
-                    'color': list(annotation.color.as_rgb_tuple(alpha=False)),
+            "properties": {
+                "classification": {
+                    "name": annotation.classification,
+                    "color": list(annotation.color.as_rgb_tuple(alpha=False)),
                 },
-                'area': annotation.geometry.area,
-                'object_type': 'annotation',
-            }
-        } for annotation in annotations]
-    return jsonify({
-        "type": "FeatureCollection",
-        "features": data
-    }), 200
+                "area": annotation.geometry.area,
+                "object_type": "annotation",
+            },
+        }
+        for annotation in annotations
+    ]
+    return jsonify({"type": "FeatureCollection", "features": data}), 200
 
 
-@blueprint.route('/viewer/<image_id:image_id>/annotations.json')
+@blueprint.route("/viewer/<image_id:image_id>/annotations.json")
 def serve_w3c_annotations(image_id):
 
     try:
@@ -286,7 +294,7 @@ def serve_w3c_annotations(image_id):
     except KeyError:
         return abort(404, f"No annotations found for {image_id!r}")
 
-    w3c_annotations = [w3c_like_annotation(annotation) for annotation in annotations] 
+    w3c_annotations = [w3c_like_annotation(annotation) for annotation in annotations]
     return jsonify(w3c_annotations), 200
 
 
@@ -299,7 +307,7 @@ def w3c_like_annotation(annotation: Annotation, prefix="anno"):
 
     region = annotation.geometry
     class_name = annotation.classification
-    safe_class_name = re.sub('[^A-Za-z0-9]+', '', class_name)
+    safe_class_name = re.sub("[^A-Za-z0-9]+", "", class_name)
 
     # remove style information from svg for w3c annotation
     _svg_style_re = re.compile(
@@ -316,19 +324,21 @@ def w3c_like_annotation(annotation: Annotation, prefix="anno"):
         "@context": "http://www.w3.org/ns/anno.jsonld",
         "id": f"{prefix}-{safe_class_name.replace(' ', '').replace(':', '')}-{uuid.uuid4()}",
         "type": "Annotation",
-        "body": [{
-            "type": "TextualBody",
-            "value": safe_class_name,
-            "purpose": "tagging",
-        }],
+        "body": [
+            {
+                "type": "TextualBody",
+                "value": safe_class_name,
+                "purpose": "tagging",
+            }
+        ],
         "motivation": "classifying",
         "creator": None,
         "created": None,
         "target": {
             "selector": {
                 "type": "SvgSelector",
-                "conformsTo" : "https://www.w3.org/TR/annotation-model/#svg-selector",
+                "conformsTo": "https://www.w3.org/TR/annotation-model/#svg-selector",
                 "value": f"<svg>{svg_path}</svg>",
             }
-        }
+        },
     }
